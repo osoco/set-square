bd62739249669560a47e26c3f3a67eadb27e741c
jlean <jose.sanleandro@ventura24.es>
2014-09-15 13:26:42 +0200

    Splitting environment settings to allow overriding.

---
+++ phusion-baseimage/build.inc.sh
@@ -0,0 +1,104 @@
+# Environment
+function defineEnv() {
+  
+  echo "defining env"
+  export AUTHOR_DEFAULT="<rydnr@acm-sl.org>";
+  export AUTHOR_DESCRIPTION="The author of the image(s) to build";
+  if    [ "${AUTHOR+1}" != "1" ] \
+     || [ "x${AUTHOR}" == "x" ]; then
+    export AUTHOR="${AUTHOR_DEFAULT}";
+  fi
+
+  export NAMESPACE_DEFAULT="rydnr";
+  export NAMESPACE_DESCRIPTION="The docker registry's namespace";
+  if    [ "${NAMESPACE+1}" != "1" ] \
+     || [ "x${NAMESPACE}" == "x" ]; then
+    export NAMESPACE="${NAMESPACE_DEFAULT}";
+  fi
+
+  export DATE_DEFAULT="$(date '+%Y%m%d')";
+  export DATE_DESCRIPTION="The date used to tag images";
+  if    [ "${DATE+1}" != "1" ] \
+     || [ "x${DATE}" == "x" ]; then
+    export DATE="${DATE_DEFAULT}";
+  fi
+
+  export MYSQL_ROOT_PASSWORD_DEFAULT="secret";
+  export MYSQL_ROOT_PASSWORD_DESCRIPTION="The default password for the root user in MySQL databases";
+  if    [ "${MYSQL_ROOT_PASSWORD+1}" != "1" ] \
+     || [ "x${MYSQL_ROOT_PASSWORD}" == "x" ]; then
+    export MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD_DEFAULT}";
+  fi
+
+  export MYSQL_ADMIN_USER_DEFAULT="admin";
+  export MYSQL_ADMIN_USER_DESCRIPTION="The name of the admin user in MySQL databases";
+  if    [ "${MYSQL_ADMIN_USER+1}" != "1" ] \
+     || [ "x${MYSQL_ADMIN_USER}" == "x" ]; then
+    export MYSQL_ADMIN_USER="${MYSQL_ADMIN_USER_DEFAULT}";
+  fi
+
+  export MYSQL_ADMIN_PASSWORD_DEFAULT="secret";
+  export MYSQL_ADMIN_PASSWORD_DESCRIPTION="The default password for the admin user in MySQL databases";
+  if    [ "${MYSQL_ADMIN_PASSWORD+1}" != "1" ] \
+     || [ "x${MYSQL_ADMIN_PASSWORD}" == "x" ]; then
+    export MYSQL_ADMIN_PASSWORD="${MYSQL_ADMIN_PASSWORD_DEFAULT}";
+  fi
+
+  export GETBOO_DB_NAME_DEFAULT="bm";
+  export GETBOO_DB_NAME_DESCRIPTION="The database name for getboo schema";
+  if    [ "${GETBOO_DB_NAME+1}" != "1" ] \
+     || [ "x${GETBOO_DB_NAME}" == "x" ]; then
+    export GETBOO_DB_NAME="${GETBOO_DB_NAME_DEFAULT}";
+  fi
+  
+  export GETBOO_DB_USERNAME_DEFAULT="bm";
+  export GETBOO_DB_USERNAME_DESCRIPTION="The name for getboo user in MySQL";
+  if    [ "${GETBOO_DB_USERNAME+1}" != "1" ] \
+     || [ "x${GETBOO_DB_USERNAME}" == "x" ]; then
+    export GETBOO_DB_USERNAME="${GETBOO_DB_USERNAME_DEFAULT}";
+  fi
+  
+  export GETBOO_DB_PASSWORD_DEFAULT="secret";
+  export GETBOO_DB_PASSWORD_DESCRIPTION="The password for getboo user in MySQL";
+  if    [ "${GETBOO_DB_PASSWORD+1}" != "1" ] \
+     || [ "x${GETBOO_DB_PASSWORD}" == "x" ]; then
+    export GETBOO_DB_PASSWORD="${GETBOO_DB_PASSWORD_DEFAULT}";
+  fi
+  
+  export GETBOO_DB_TABLE_PREFIX_DEFAULT="";
+  export GETBOO_DB_TABLE_PREFIX_DESCRIPTION="The prefix used for all tables in getboo database";
+  if    [ "${GETBOO_DB_TABLE_PREFIX+1}" != "1" ] \
+     || [ "x${GETBOO_DB_TABLE_PREFIX}" == "x" ]; then
+    export GETBOO_DB_TABLE_PREFIX="${GETBOO_DB_TABLE_PREFIX_DEFAULT}";
+  fi
+  
+  export GETBOO_DOMAIN_DEFAULT="bm.acm-sl.org";
+  export GETBOO_DOMAIN_DESCRIPTION="The domain getboo will be serving";
+  if    [ "${GETBOO_DOMAIN+1}" != "1" ] \
+     || [ "x${GETBOO_DOMAIN}" == "x" ]; then
+    export GETBOO_DOMAIN="${GETBOO_DOMAIN_DEFAULT}";
+  fi
+
+  export HTTPS_DOMAIN_DEFAULT="${GETBOO_DOMAIN}";
+  export HTTPS_DOMAIN_DESCRIPTION="The https domain";
+  if    [ "${HTTPS_DOMAIN+1}" != "1" ] \
+     || [ "x${HTTPS_DOMAIN}" == "x" ]; then
+    export HTTPS_DOMAIN="${HTTPS_DOMAIN_DEFAULT}";
+  fi
+
+  ENV_VARIABLES=(\
+    AUTHOR \
+    NAMESPACE \
+    MYSQL_ROOT_PASSWORD \
+    MYSQL_ADMIN_USER \
+    MYSQL_ADMIN_PASSWORD \
+    GETBOO_DB_NAME \
+    GETBOO_DB_USERNAME \
+    GETBOO_DB_PASSWORD \
+    GETBOO_DB_TABLE_PREFIX \
+    GETBOO_DOMAIN \
+    HTTPS_DOMAIN \
+  );
+ 
+  export ENV_VARIABLES;
+}
--- phusion-baseimage/build.sh
+++ phusion-baseimage/build.sh
@@ -25,126 +25,6 @@ function checkRequirements() {
   checkReq envsubst ENVSUBST_NOT_INSTALLED;
 }
  
-# Environment
-function defineEnv() {
-  
-  export AUTHOR_DEFAULT="<rydnr@acm-sl.org>";
-  export AUTHOR_DESCRIPTION="The author of the image(s) to build";
-  if    [ "${AUTHOR+1}" != "1" ] \
-     || [ "x${AUTHOR}" == "x" ]; then
-    export AUTHOR="${AUTHOR_DEFAULT}";
-  fi
-
-  export NAMESPACE_DEFAULT="rydnr";
-  export NAMESPACE_DESCRIPTION="The docker registry's namespace";
-  if    [ "${NAMESPACE+1}" != "1" ] \
-     || [ "x${NAMESPACE}" == "x" ]; then
-    export NAMESPACE="${NAMESPACE_DEFAULT}";
-  fi
-
-  export DATE_DEFAULT="$(date '+%Y%m%d')";
-  export DATE_DESCRIPTION="The date used to tag images";
-  if    [ "${DATE+1}" != "1" ] \
-     || [ "x${DATE}" == "x" ]; then
-    export DATE="${DATE_DEFAULT}";
-  fi
-
-  export TOMCAT_VERSION_DEFAULT="8.0.12";
-  export TOMCAT_VERSION_DESCRIPTION="The version of the Tomcat server";
-  if    [ "${TOMCAT_VERSION+1}" != "1" ] \
-     || [ "x${TOMCAT_VERSION}" == "x" ]; then
-    export TOMCAT_VERSION="${TOMCAT_VERSION_DEFAULT}";
-  fi
-  
-  export NLP_WEBAPP_TAG_DEFAULT="latest-SNAPSHOT";
-  export NLP_WEBAPP_TAG_DESCRIPTION="The version of nlp-webapp-war to release";
-  if    [ "${NLP_WEBAPP_TAG+1}" != "1" ] \
-     || [ "x${NLP_WEBAPP_TAG}" == "x" ]; then
-    export NLP_WEBAPP_TAG="${NLP_WEBAPP_TAG_DEFAULT}";
-  fi
-  
-  export MYSQL_ROOT_PASSWORD_DEFAULT="secret";
-  export MYSQL_ROOT_PASSWORD_DESCRIPTION="The default password for the root user in MySQL databases";
-  if    [ "${MYSQL_ROOT_PASSWORD+1}" != "1" ] \
-     || [ "x${MYSQL_ROOT_PASSWORD}" == "x" ]; then
-    export MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD_DEFAULT}";
-  fi
-
-  export MYSQL_ADMIN_USER_DEFAULT="admin";
-  export MYSQL_ADMIN_USER_DESCRIPTION="The name of the admin user in MySQL databases";
-  if    [ "${MYSQL_ADMIN_USER+1}" != "1" ] \
-     || [ "x${MYSQL_ADMIN_USER}" == "x" ]; then
-    export MYSQL_ADMIN_USER="${MYSQL_ADMIN_USER_DEFAULT}";
-  fi
-
-  export MYSQL_ADMIN_PASSWORD_DEFAULT="secret";
-  export MYSQL_ADMIN_PASSWORD_DESCRIPTION="The default password for the admin user in MySQL databases";
-  if    [ "${MYSQL_ADMIN_PASSWORD+1}" != "1" ] \
-     || [ "x${MYSQL_ADMIN_PASSWORD}" == "x" ]; then
-    export MYSQL_ADMIN_PASSWORD="${MYSQL_ADMIN_PASSWORD_DEFAULT}";
-  fi
-
-  export GETBOO_DB_NAME_DEFAULT="bm";
-  export GETBOO_DB_NAME_DESCRIPTION="The database name for getboo schema";
-  if    [ "${GETBOO_DB_NAME+1}" != "1" ] \
-     || [ "x${GETBOO_DB_NAME}" == "x" ]; then
-    export GETBOO_DB_NAME="${GETBOO_DB_NAME_DEFAULT}";
-  fi
-  
-  export GETBOO_DB_USERNAME_DEFAULT="bm";
-  export GETBOO_DB_USERNAME_DESCRIPTION="The name for getboo user in MySQL";
-  if    [ "${GETBOO_DB_USERNAME+1}" != "1" ] \
-     || [ "x${GETBOO_DB_USERNAME}" == "x" ]; then
-    export GETBOO_DB_USERNAME="${GETBOO_DB_USERNAME_DEFAULT}";
-  fi
-  
-  export GETBOO_DB_PASSWORD_DEFAULT="secret";
-  export GETBOO_DB_PASSWORD_DESCRIPTION="The password for getboo user in MySQL";
-  if    [ "${GETBOO_DB_PASSWORD+1}" != "1" ] \
-     || [ "x${GETBOO_DB_PASSWORD}" == "x" ]; then
-    export GETBOO_DB_PASSWORD="${GETBOO_DB_PASSWORD_DEFAULT}";
-  fi
-  
-  export GETBOO_DB_TABLE_PREFIX_DEFAULT="";
-  export GETBOO_DB_TABLE_PREFIX_DESCRIPTION="The prefix used for all tables in getboo database";
-  if    [ "${GETBOO_DB_TABLE_PREFIX+1}" != "1" ] \
-     || [ "x${GETBOO_DB_TABLE_PREFIX}" == "x" ]; then
-    export GETBOO_DB_TABLE_PREFIX="${GETBOO_DB_TABLE_PREFIX_DEFAULT}";
-  fi
-  
-  export GETBOO_DOMAIN_DEFAULT="bm.acm-sl.org";
-  export GETBOO_DOMAIN_DESCRIPTION="The domain getboo will be serving";
-  if    [ "${GETBOO_DOMAIN+1}" != "1" ] \
-     || [ "x${GETBOO_DOMAIN}" == "x" ]; then
-    export GETBOO_DOMAIN="${GETBOO_DOMAIN_DEFAULT}";
-  fi
-
-  export HTTPS_DOMAIN_DEFAULT="${GETBOO_DOMAIN}";
-  export HTTPS_DOMAIN_DESCRIPTION="The https domain";
-  if    [ "${HTTPS_DOMAIN+1}" != "1" ] \
-     || [ "x${HTTPS_DOMAIN}" == "x" ]; then
-    export HTTPS_DOMAIN="${HTTPS_DOMAIN_DEFAULT}";
-  fi
-
-  ENV_VARIABLES=(\
-    AUTHOR \
-    NAMESPACE \
-    TOMCAT_VERSION \
-    NLP_WEBAPP_TAG \
-    MYSQL_ROOT_PASSWORD \
-    MYSQL_ADMIN_USER \
-    MYSQL_ADMIN_PASSWORD \
-    GETBOO_DB_NAME \
-    GETBOO_DB_USERNAME \
-    GETBOO_DB_PASSWORD \
-    GETBOO_DB_TABLE_PREFIX \
-    GETBOO_DOMAIN \
-    HTTPS_DOMAIN \
-  );
- 
-  export ENV_VARIABLES;
-}
-
 # Error messages
 function defineErrors() {
   export INVALID_OPTION="Unrecognized option";
@@ -273,6 +153,9 @@ function repo_exists() {
   fi
 }
 
+[ -e "$(basename ${SCRIPT_NAME} .sh).inc.sh" ] && source "$(basename ${SCRIPT_NAME} .sh).inc.sh"
+
+[ -e "$(basename ${SCRIPT_NAME} .sh).inc.sh" ] && echo "$(basename ${SCRIPT_NAME} .sh).inc.sh"
 function main() {
   local _repo;
   for _repo in ${REPOS}; do
