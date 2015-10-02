7acd4f2600bdf9d9537a2a5611bcd08ea15bedaa
chous <chous@acm-sl.org>
2015-10-02 08:57:28 +0200

    Working on MariaDB image
    - Fixed minor bugs in the script to process SQL files on demand, at runtime, but it's not yet fully working.
    
    Some work on tt-rss image.

--- tt-rss/build-settings.sh
+++ tt-rss/build-settings.sh
@@ -1,1 +1,4 @@
 defineEnvVar TTRSS_VIRTUAL_HOST "The virtual host of the tt-rss installation" "rss.acm-sl.org";
+defineEnvVar TTRSS_DB_NAME "The database name" "ttrss";
+defineEnvVar TTRSS_DB_USER "The database user" "ttrss";
+defineEnvVar TTRSS_DB_PASSWORD "The database password" '${RANDOM_PASSWORD}';
