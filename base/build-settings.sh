2f1df8c656f8c99d0f6e6578ed573b5fc2597ca9
chous <chous@acm-sl.org>
2015-10-14 09:00:32 +0200

    Removed logstash-agent from base image.

--- base/build-settings.sh
+++ base/build-settings.sh
@@ -1,13 +1,6 @@
-#defineEnvVar LOGGLY_TOKEN "The loggly.com API token" "token"
-defineEnvVar JAVA_VERSION \
-             "The Java version" \
-             "8";
-defineEnvVar JCE_VERSION \
-             "The JCE version" \
-             '${JAVA_VERSION}';
-defineEnvVar JCE_DOWNLOAD_URL \
-             "The url to download the JCE" \
-             'http://download.oracle.com/otn-pub/java/jce/${JCE_VERSION}/jce_policy-${JCE_VERSION}.zip';
-defineEnvVar LOGSTASH_HEAP_SIZE \
-             "The heap size of logstash agent. Default is 500" \
-             "256"
+defineEnvVar LOGSERVER_HOST \
+             "The logserver host" \
+             'logger';
+defineEnvVar LOGSERVER_PORT \
+             "The logserver port" \
+             "514";
--- base/logstash-cron.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec /opt/logstash/bin/logstash agent -f /etc/logstash/conf.d/cron.conf
