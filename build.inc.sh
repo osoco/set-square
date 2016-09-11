84742296b9b8351755e7206c49098f239300c2fc
Jose San Leandro <jose.sanleandro@osoco.es>
2016-09-11 08:59:14 +0200

    Minor change

--- build.inc.sh
+++ build.inc.sh
@@ -4,6 +4,10 @@ defineEnvVar AUTHOR_EMAIL "The author's email" 'me@${DOMAIN}';
 defineEnvVar NAMESPACE "The docker registry's namespace" "example";
 defineEnvVar DATE "The date format used to tag images" "$(date '+%Y%m')";
 defineEnvVar TIME "A timestamp" "$(date)";
+defineEnvVar REGISTRY "The registry to push Docker images to" "tutum.co";
+defineEnvVar REGISTRY_NAMESPACE \
+             "The namespace under the registry where the image is to be uploaded" \
+             '${NAMESPACE}';
 defineEnvVar ROOT_IMAGE_VERSION "The root image version" "0.9.18";
 defineEnvVar ROOT_IMAGE_64BIT "The default root image for 64 bits" "phusion/baseimage"
 defineEnvVar RANDOM_PASSWORD \
@@ -13,10 +17,6 @@ defineEnvVar RANDOM_PASSWORD \
 defineEnvVar ROOT_IMAGE_32BIT "The default root image for 32 bits" "${ROOT_IMAGE_64BIT_DEFAULT}32";
 defineEnvVar BASE_IMAGE_64BIT "The base image for 64 bits" '${NAMESPACE}/base';
 defineEnvVar BASE_IMAGE_32BIT "The base image for 32 bits" '${BASE_IMAGE_64BIT_DEFAULT%%64}32';
-defineEnvVar REGISTRY "The registry to push Docker images to" "tutum.co";
-defineEnvVar REGISTRY_NAMESPACE \
-             "The namespace under the registry where the image is to be uploaded" \
-             '${NAMESPACE}';
 defineEnvVar JAVA_VERSION "The Java version" "8";
 defineEnvVar APTGET_INSTALL \
              "Installs a program via apt-get" \
