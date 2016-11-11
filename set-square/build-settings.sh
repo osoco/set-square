f25d69d1710794897c9edb2bdaa09e8e9821e876
Jose San Leandro <jose.sanleandro@osoco.es>
2016-11-11 13:58:59 +0100

    WIP with the gradle settings

--- set-square/build-settings.sh
+++ set-square/build-settings.sh
@@ -2,3 +2,6 @@ defineEnvVar SERVICE_USER "The service user" "setsquare";
 defineEnvVar SERVICE_GROUP "The service group" "users";
 defineEnvVar SERVICE_USER_HOME 'The home of ${SERVICE_USER} user' '/home/${SERVICE_USER}';
 defineEnvVar SERVICE_USER_SHELL 'The shell of ${SERVICE_USER} user' '/bin/bash';
+defineEnvVar DOCKER_USERNAME "The docker account username" '${AUTHOR}';
+defineEnvVar DOCKER_PASSWORD "The docker account password" '${RANDOM_PASSWORD}';
+defineEnvVar DOCKER_EMAIL "The docker account email" '${AUTHOR_EMAIL}';
