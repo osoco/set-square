68fd6225ec60852773255396803f176adac8722b
Jose San Leandro <josesla@ext.inditex.com>
2020-01-22 06:50:30 +0100

    Skipping DWIFS from template processing since it triggers a bug I am not able to fix so far

--- .templates/common-files/process-file.sh
+++ .templates/common-files/process-file.sh
@@ -5,11 +5,19 @@
 # api: public
 # txt: Processes a file, replacing any placeholders with the contents of the environment variables, and stores the result in the specified output file.
 
+# fun: main
+# api: public
+# txt: Main logic (dry-wit hook).
+# txt: Retuns 0/TRUE always.
+function main() {
+  replace_placeholders "${INPUT_FILE}" "${OUTPUT_FILE}";
+}
+
 # fun: replace_placeholders
 # api: public
 # txt: Replaces any placeholders in given file.
 # opt: input: The file to process.
-# opt: output:: The output file.
+# opt: output: The output file.
 # txt: Returns 0/TRUE if the file is processed, 1/FALSE otherwise.
 # txt: The variable RESULT contains the path of the processed file if the function returns 0/TRUE.
 function replace_placeholders() {
@@ -19,32 +27,31 @@ function replace_placeholders() {
   checkNotEmpty file "${_file}" 1;
   checkNotEmpty output "${_output}" 2;
 
-  local _env="$(IFS=" \t" env | awk -F'=' '{printf("%s=\"%s\" ", $1, $2);}')";
+  local _env="$(IFS=" \t" env | grep -v 'DWIFS' | awk -F'=' '{printf("%s=\"%s\" ", $1, $2);}')";
   replaceVariablesInFile "${_file}" "${_output}" ${_env};
-}
-
-# fun: main
-# api: public
-# txt: Main logic.
-# txt: Retuns 0/TRUE always
-# use: main
-function main() {
-  replace_placeholders "${INPUT_FILE}" "${OUTPUT_FILE}";
 }
 
 ## Script metadata and CLI options
 setScriptDescription "Processes a file, replacing any placeholders with the contents of the \
 environment variables, and stores the result in the specified output file.";
 
-addCommandLineFlag "output" "o" "The command" MANDATORY EXPECTS_ARGUMENT;
+addCommandLineFlag "output" "o" "The output file" MANDATORY EXPECTS_ARGUMENT;
 addCommandLineParameter "input" "The input file" MANDATORY SINGLE;
 
 checkReq envsubst;
 
+# fun: dw_parse_input_cli_parameter
+# api: public
+# txt: Parses the "input" parameter (dry-wit hook).
+# txt: Returns 0/TRUE always.
 function dw_parse_input_cli_parameter() {
   export INPUT_FILE="${1}";
 }
 
+# fun: dw_parse_output_cli_flag
+# api: public
+# txt: Parses the "output" flag (dry-wit hook).
+# txt: Returns 0/TRUE always.
 function dw_parse_output_cli_flag() {
   export OUTPUT_FILE="${1}";
 }
